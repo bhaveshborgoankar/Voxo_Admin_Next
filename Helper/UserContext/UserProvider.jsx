9f37ccab1b25247fb022359a030344fc7df58c9b
bhaveshborgoankar <bhavesh.borgoankar@webiots.com>
2022-12-26 23:49:29 +0530

    Update the middleware

--- Helper/UserContext/UserProvider.jsx
+++ Helper/UserContext/UserProvider.jsx
@@ -1,17 +1,12 @@
 import React, { useState } from 'react';
-import { useCookies } from 'react-cookie';
 import UserContext from '.';
 
 const UserProvider = (props) => {
   const [userEdit, setUserEdit] = useState({});
   const [active, setActive] = useState({ status: false, title: '' });
-  const [uat, setUat, removeUat] = useCookies(['uat']);
-  const [fpEmailCookie, setFpEmailCookie, removeFpEmailCookie] = useCookies(['fpemail']);
-  const [voCookie, setVoCookie, removeVoCookie] = useCookies(['vo']);
-  console.log('In contexyt', uat, '=>>', setUat);
   return (
     <UserContext.Provider
-      value={{ uat, setUat, removeUat, userEdit, setUserEdit, fpEmailCookie, setFpEmailCookie, removeFpEmailCookie, voCookie, setVoCookie, removeVoCookie, active, setActive, ...props }}>
+      value={{ userEdit, setUserEdit, active, setActive, ...props }}>
       {props.children}
     </UserContext.Provider>
   );
--- middleware.js
+++ middleware.js
@@ -1,15 +1,13 @@
 import { NextResponse } from 'next/server';
 
 export const middleware = (request) => {
-    // const url = request.nextUrl
+    console.log("request", request)
     const cookie = request?.cookies.has("uat")
-    console.log("cookie", "request", request?.cookies.has("uat"));
-    if (cookie) {
-        console.log("gdgdgdgdgd", request.url);
+    if (!cookie) {
         return NextResponse.redirect(new URL('/auth/login', request.url))
     } else NextResponse.next()
 };
 
 export const config = {
-    matcher: ["/auth/:path*"],
+    matcher: ["/users/:path*", "/"],
 };
